b99c4be3233c9c9c163350ed41dc2aea00959c29
Syedsamid <samidsyed1720@gmail.com>
2025-02-24 12:55:52 +0000

    setup

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <>
+            <div className="flex items-center justify-center min-h-screen bg-gray-100 p-4">
+                <div className="bg-white shadow-lg rounded-2xl p-6 max-w-sm w-full text-center">
+                    <h2 className="text-xl font-semibold text-gray-800">Welcome to home</h2>
+                    <p className='text-gray-600 mt-2'> This is the home page.</p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Home
